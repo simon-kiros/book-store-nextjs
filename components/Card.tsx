09ca003f5b387f4b574b48be15e5aa114a751f78
Simon Kiros <simonkiros007@gmail.com>
2023-08-02 19:45:25 +0300

    commit 3

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 type PropType = {
   title: string;
@@ -9,17 +9,17 @@ type PropType = {
 
 function Card({ title, discount, cover, price }: PropType) {
   return (
-    <div className="card" style={{ width: '150px' }}>
+    <div className="card" style={{ width: "150px" }}>
       <div className="flex flex-col">
         <div className="">
-          <img className="card-image" src={cover} style={{ width: '210px', height: '210px' }} />
+          <img className="card-image" src={cover} style={{ width: "210px", height: "210px" }} />
         </div>
         <div className="title px-2">
           <h2>{title}</h2>
         </div>
         <div className="flex flex-row px-2 justify-between font-bold">
-          <span className="text-red-600">10%</span>
-          <span>10</span>
+          <span className="text-red-600">10% {price}</span>
+          <span>10 {discount}</span>
         </div>
       </div>
     </div>
--- pages/books.tsx
+++ pages/books.tsx
@@ -1,100 +1,83 @@
-'use client';
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Head from 'next/head';
-import { useQuery } from '@tanstack/react-query';
-import Card from '@/components/Card';
-import Link from 'next/link';
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import Card from "@/components/Card";
+import Link from "next/link";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 const endPoint = `https://15.165.74.54:3000/?page=`;
 //const endPoint = `https://dummyjson.com/todos`;
 
-
 function Books() {
-  const [hasMore, setHasMore] = useState(true);
+  const [hasMore] = useState(true);
   const [books, setBooks] = useState([]);
   const [page, setPage] = useState<number>(1);
-  const { isLoading, isError, data, error } = useQuery({
-    queryKey: ['books'],
+  const { isLoading, data, error } = useQuery({
+    queryKey: ["books"],
     queryFn: async () =>
       await axios
         .get(endPoint + page)
         .then((res) => {
-          console.log('success happed');
+          console.log("success happed");
           console.dir(res.data);
-          setBooks(res.data);
+          //setBooks(res.data);
+          return res.data;
         })
         .catch((error) => {
-          console.log('something happ');
+          console.log("something happ");
           console.log(error);
         }),
   });
 
-
   async function fetchMoreData() {
-    setPage(page + 1)
-    const url = endPoint + (page + 1) 
+    setPage(page + 1);
+    const url = endPoint + (page + 1);
     await axios
-        .get(url)
-        .then((res) => {
-          console.log('success happed');
-          console.dir(res.data);
-          setBooks(res.data);
-        })
-        .catch((error) => {
-          console.log('something happ');
-          console.log(error);
-        });
-  }
-
-  if (isLoading) return <span>Loading...</span>;
-
-  //if (isError) return <span>Error: happend</span>;
-  if (error instanceof Error) return <div>{error.message }</div>;
-  function aaa() {
-    fetch(endPoint)
-      .then((response) => {
-        //handle response
-        console.log(response);
-      })
-      .then((data) => {
-        //handle data
-        console.log(data);
+      .get(url)
+      .then((res) => {
+        console.log("success happed");
+        console.dir(res.data);
+        setBooks(res.data);
       })
       .catch((error) => {
-        //handle error
+        console.log("something happ");
+        console.log(error);
       });
   }
 
-  // useEffect(() => {
-  //   //searchData();
-  // }, []);
+  useEffect(() => {
+    if (data) setBooks(data);
+  }, [isLoading]);
+
+  if (isLoading) return <span>Loading...</span>;
+
+  if (error instanceof Error) return <div>{error.message}</div>;
+  //if (isError) return <span>Error: happend</span>;
 
   return (
     <>
       <span className="font-bold flex justify-center pt-5 pb-5">Books</span>
       <div className="flex flex-row justify-center pl-20 pr-20">
         <div className="flex flex-wrap gap-1">
-        <InfiniteScroll
+          <InfiniteScroll
             dataLength={books.length}
             next={fetchMoreData}
             hasMore={hasMore}
             scrollThreshold={0.8}
             loader={<p>...</p>}>
-          {books.map((t: string, i: number) => (
-            <Link href={`/1/detail`} key={i}>
-              <Card
-                key={i}
-                title="This is book"
-                discount={10}
-                cover="https://images-na.ssl-images-amazon.com/images/I/51Ga5GuElyL._AC_SX184_.jpg"
-                price={100}
-              />
-            </Link>
-          ))}
+            {books.map((t: string, i: number) => (
+              <Link href={`/1/detail`} key={i}>
+                <Card
+                  key={i}
+                  title="This is book"
+                  discount={10}
+                  cover="https://images-na.ssl-images-amazon.com/images/I/51Ga5GuElyL._AC_SX184_.jpg"
+                  price={100}
+                />
+              </Link>
+            ))}
           </InfiniteScroll>
-          
         </div>
       </div>
     </>
