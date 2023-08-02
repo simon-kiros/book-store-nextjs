7d74fb6cd7da116bce22441d87f0efb4855f5c70
simon k <samvfx016@gmail.com>
2023-08-02 21:59:56 +0300

    commit 7

--- pages/books.tsx
+++ pages/books.tsx
@@ -9,15 +9,22 @@ import InfiniteScroll from "react-infinite-scroll-component";
 const endPoint = `http://15.165.74.54:3000/?page=`;
 //const endPoint = `https://dummyjson.com/todos`;
 
+type Book = {
+  title: string;
+  description: string;
+  discountRate: number;
+  coverImage: string;
+  price: number;
+}
 function Books() {
-  const [hasMore] = useState(true);
-  const [books, setBooks] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+  const [books, setBooks] = useState<Book[]>([]);
   const [page, setPage] = useState<number>(1);
   const { isLoading, data, error } = useQuery({
     queryKey: ["books"],
     queryFn: async () =>
       await axios
-        .get(endPoint + page)
+        .get<Book[]>(endPoint + page)
         .then((res) => {
           console.log("success happed useQuery");
           console.dir(res.data);
@@ -31,18 +38,19 @@ function Books() {
   });
 
   async function fetchMoreData() {
-    setPage(page + 1);
+    setPage(7);
     const url = endPoint + (page + 1);
     console.log("page : ", url);
     await axios
-      .get(url)
+      .get<Book[]>(url)
       .then((res) => {
         console.log("success happed");
         console.log("-- in fetchMoreData");
-        console.dir(res.data.data);
+        console.dir(res);
         console.log("-- ----");
-        const a: any[] = res.data.data;
-        setBooks([...books]);
+        //const a: any[] = res.data.data;
+        if (res.data === "") setHasMore(false);
+        else setBooks([...books, ...res.data.data]);
       })
       .catch((error) => {
         console.log("something happ");
@@ -67,14 +75,14 @@ function Books() {
     <>
       <span className="font-bold flex justify-center pt-5 pb-5">Books</span>
       <div className="flex flex-row justify-center pl-20 pr-20">
-        <div className="flex flex-wrap gap-1">
+        <div className="flex flex-wrap gap-4">
           <InfiniteScroll
             dataLength={books?.length}
             next={fetchMoreData}
             hasMore={hasMore}
             scrollThreshold={0.8}
             loader={<p>...</p>}>
-            {books?.map((t: string, i: number) => (
+            {books?.map((t: Book, i: number) => (
               <Link href={`/1/detail`} key={i}>
                 <Card
                   key={i}
